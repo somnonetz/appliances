58eff569f39a1604fa82d38a2a2b68108d071212
jmz-b <jaymuhz.bee@gmail.com>
2020-09-03 11:27:21 +0200

    snet-nginx: add new keycloak vars

--- snet-nginx/entrypoint.sh
+++ snet-nginx/entrypoint.sh
@@ -7,12 +7,14 @@ find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_C
 find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_CLIENT_TA_URL},${SSE_CLIENT_TA_URL},g" {} \; 
 find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_CLIENT_SALT_VALUE},${SSE_CLIENT_SALT_VALUE},g" {} \;
 find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_CLIENT_IV_VALUE},${SSE_CLIENT_IV_VALUE},g" {} \;
+find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" {} \;
+find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${KEYCLOAK_REALM},${KEYCLOAK_REALM},g" {} \;
+find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${KEYCLOAK_CLIENTID},${KEYCLOAK_CLIENTID},g" {} \;
 
 sed -i "s,\${SSE_CLIENT_SSE_SERVER_URL},${SSE_CLIENT_SSE_SERVER_URL},g" /var/www/asclepios-search/lib/sse.js;
 sed -i "s,\${SSE_CLIENT_TA_URL},${SSE_CLIENT_TA_URL},g" /var/www/asclepios-search/lib/sse.js; 
 sed -i "s,\${SSE_CLIENT_SALT_VALUE},${SSE_CLIENT_SALT_VALUE},g" /var/www/asclepios-search/lib/sse.js;
 sed -i "s,\${SSE_CLIENT_IV_VALUE},${SSE_CLIENT_IV_VALUE},g" /var/www/asclepios-search/lib/sse.js;
-sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" /var/www/asclepios-search/index.html;
-sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" /var/www/asclepios-search/keycloak.json;
+find /var/www/asclepios-search/ -type f -exec sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" {} \;
 
 exec "$@"
