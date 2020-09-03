f7a97865551f7b26e6b238ab942548d85d856eb5
jmz-b <jaymuhz.bee@gmail.com>
2020-09-03 13:42:57 +0200

    snet-nginx: switch to multi-stage build + git

--- snet-nginx/entrypoint.sh
+++ snet-nginx/entrypoint.sh
@@ -8,13 +8,12 @@ find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_C
 find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_CLIENT_SALT_VALUE},${SSE_CLIENT_SALT_VALUE},g" {} \;
 find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${SSE_CLIENT_IV_VALUE},${SSE_CLIENT_IV_VALUE},g" {} \;
 find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" {} \;
-find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${KEYCLOAK_REALM},${KEYCLOAK_REALM},g" {} \;
-find /var/www/copla-editor/sn-editor/static/js/ -type f -exec sed -i "s,\${KEYCLOAK_CLIENTID},${KEYCLOAK_CLIENTID},g" {} \;
 
 sed -i "s,\${SSE_CLIENT_SSE_SERVER_URL},${SSE_CLIENT_SSE_SERVER_URL},g" /var/www/asclepios-search/lib/sse.js;
 sed -i "s,\${SSE_CLIENT_TA_URL},${SSE_CLIENT_TA_URL},g" /var/www/asclepios-search/lib/sse.js; 
 sed -i "s,\${SSE_CLIENT_SALT_VALUE},${SSE_CLIENT_SALT_VALUE},g" /var/www/asclepios-search/lib/sse.js;
 sed -i "s,\${SSE_CLIENT_IV_VALUE},${SSE_CLIENT_IV_VALUE},g" /var/www/asclepios-search/lib/sse.js;
-find /var/www/asclepios-search/ -type f -exec sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" {} \;
+sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" /var/www/asclepios-search/keycloak.json;
+sed -i "s,\${KEYCLOAK_HOST},${KEYCLOAK_HOST},g" /var/www/asclepios-search/index.html;
 
 exec "$@"
