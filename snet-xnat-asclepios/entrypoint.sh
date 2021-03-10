c960977c9ad6547cbf006ae923fe260c56956b4e
jmz-b <jaymuhz.bee@gmail.com>
2021-03-10 10:26:37 +0100

    snet-xnat-asclepios: bump to xnat 1.8

--- snet-xnat-asclepios/entrypoint.sh
+++ snet-xnat-asclepios/entrypoint.sh
@@ -31,8 +31,20 @@ hibernate.hbm2ddl.auto=update
 hibernate.show_sql=false
 hibernate.cache.use_second_level_cache=true
 hibernate.cache.use_query_cache=true
+
+
+spring.http.multipart.max-file-size=1073741824
+spring.http.multipart.max-request-size=1073741824
 EOF
 fi
+
+if [ ! -z "${XNAT_EMAIL}" ]; then
+  cat > ${XNAT_HOME}/config/prefs-init.ini << EOF
+[siteConfig]
+adminEmail=${XNAT_EMAIL}
+EOF
+fi
+
 
 # wait for postgres
 until psql -U "$XNAT_DATASOURCE_USERNAME" -h xnat-db -c '\q'; do
